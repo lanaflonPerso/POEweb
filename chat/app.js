15caf61398d52b4f3990a9bcedb3e9029a37d85d
baptistehun <hun.baptiste@orange.fr>
2018-07-19 15:32:25 +0200

    ChatSolo
    ChatSolo

---
+++ chat/app.js
@@ -0,0 +1,29 @@
+﻿var Chat = {
+    username: '',
+    connect: function () {
+        var self = this;
+        self.username = $('#wrapperId #pseudoInput').val();
+        $('#wrapperId').remove();
+        $.ajax({
+            url: 'index.html',
+            dataType: 'html',
+            success: function (r, s) {
+                $('body').html(r);
+                $('#userList').append('<li> ' + self.username + '</li>');
+            }
+        });
+        
+    }
+}
+
+var Message = {
+    username: '',
+    message: '',
+    write: function () {
+        var self = this;
+        self.message = $('#inputMessage form input[type="text"]').val();
+        self.username = Chat.username;
+        $('#allMessages').append('<div class="message"><h1> ' + self.username + '</h1><p> ' + self.message + '</p></div>')
+        $('#inputMessage form input[type="text"]').val('');
+    }
+}
---
+++ chat/listeners.js
@@ -0,0 +1,9 @@
+﻿$('body').on('submit', 'form', function (e) {
+    if (this.id == "connectId") {
+        Chat.connect();
+    }
+    else {
+        Message.write();
+    }
+    return false;
+})
